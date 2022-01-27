81941b0c4fa187e69abe233e517fa20881e855ab
Abishek Bupathi <abishekb128@gmail.com>
2022-01-27 19:20:13 +0000

    Added methods to SurveyServer

--- src/SurveyServer.java
+++ src/SurveyServer.java
@@ -1,2 +1,12 @@
+import java.rmi.*;
+import java.util.ArrayList;
+
 public interface SurveyServer {
+
+    ArrayList<QuestionnaireSummary> downloadQuestionnaireSummary() throws RemoteException;
+
+    Questionnaire downloadQuestionnaire(int questionnaireId) throws  RemoteException;
+
+    void submitQuestionnaire(Questionnaire questionnaire) throws  RemoteException;
+
 }
